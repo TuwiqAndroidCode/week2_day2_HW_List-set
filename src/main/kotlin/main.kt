9a3bd1df01270ffb143b2cd8faa60257830f9d91
Nora <Norah.Nahedh.ruh.k@tuwaiq.edu.sa>
2021-10-20 05:31:03 +0300

    //optional question 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,7 +14,18 @@ fun main (){
     // println(numbers)
     numbers.removeIf { num-> num.equals(12) }
     println(numbers)
-    //optional question
+    //optional question1
     val fileReader = File("data/data.text").readText().split("\n")
     println(fileReader)
+   //optional question 2
+    var nums = mutableListOf(1 ,66,0,33,55)
+    var max = nums[nums.size-1]
+    println(max)
+    for (index in 0 until nums.size){
+        if (max < nums[index]){
+            max=nums[index]
+        }
+    }
+    println(max)
+
 }
